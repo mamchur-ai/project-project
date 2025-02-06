d24972bea1a9f4bca0476ec15b95833bbec2a676
antonmamchur <toxicusdraw@gmail.com>
2025-02-06 12:02:20 +0500

    fix button types

--- src/Button.tsx
+++ src/Button.tsx
@@ -4,7 +4,7 @@ import goyda from './assets/goyda.mp3'
 export const Button = () => {
     const [play] = useSound(goyda)
     return (
-        <button onClick={play}>
+        <button  onClick={() => play()}>
             ГОЙДА
         </button>
     )
