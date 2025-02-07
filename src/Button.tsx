ae4d872fe1cadeaf067b512c57b18b78b49f9d5e
antonmamchur <toxicusdraw@gmail.com>
2025-02-07 09:40:03 +0500

    ios sound

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,7 +2,7 @@ import useSound from 'use-sound';
 import goyda from './assets/goyda.mp3'
 
 export const Button = () => {
-    const [play] = useSound(goyda)
+    const [play] = useSound(goyda, {html5: true})
     return (
         <button className='btn'  onClick={() => play()}>
             ГОЙДА
